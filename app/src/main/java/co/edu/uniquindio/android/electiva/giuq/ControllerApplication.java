637bd41c206fb3c774bb7d464fc5c9310962cf51
Alejandro <alejandrodxf@gmail.com>
2017-05-05 09:04:35 -0500

    Creación layout activity_login

---
+++ app/src/main/java/co/edu/uniquindio/android/electiva/giuq/ControllerApplication.java
@@ -0,0 +1,22 @@
+package co.edu.uniquindio.android.electiva.giuq;
+
+import android.app.Application;
+
+/**
+ * Controlador Aplicación
+ * @author Francisco Alejandro Hoyos Rojas
+ * @version 1.0
+ */
+
+public class ControllerApplication extends Application{
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+
+}
--- app/src/main/java/co/edu/uniquindio/android/electiva/giuq/LoginActivity.java
+++ app/src/main/java/co/edu/uniquindio/android/electiva/giuq/LoginActivity.java
@@ -1,13 +1,71 @@
 package co.edu.uniquindio.android.electiva.giuq;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
 
+/**
+ * Clase utilizada para el ingreso de los usuarios a la aplicación
+ * @author Francisco Alejandro Hoyos Rojas
+ * @version 1.0
+ */
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    /**
+     * Atributo que representa el campo de texto email
+     */
+    @BindView(R.id.editTextEmailLogin)
+    EditText editTextEmail;
+
+    /**
+     * Atributo que representa el campo de texto password
+     */
+    @BindView(R.id.editTextPassword)
+    EditText editTextPassword;
+
+    /**
+     * Atributo que representa el botón Sign Up
+     */
+    @BindView(R.id.buttonSignUp)
+    Button buttonSignUp;
+
+    /**
+     * Atributo que representa el botón Sign In
+     */
+    @BindView(R.id.buttonSignIn)
+    Button buttonSignIn;
+
+    /**
+     * Atributo que representa el botón Forgot Password
+     */
+    @BindView(R.id.buttonForgotPassword)
+    Button buttonForgotPassword;
+
+    /**
+     * Método que se encarga de realizar la conexión con la parte lógica
+     *
+     * @param savedInstanceState información a ser recepcionada
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+
+    /**
+     * Método encargado de escuchar los eventos del botón Sign Up, Sign In y Forgot Password
+     *
+     * @param v control que ejecuta el evento
+     */
+    @Override
+    public void onClick(View v) {
+
+
     }
 }
